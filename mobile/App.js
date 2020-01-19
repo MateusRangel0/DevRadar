bb1db9aa6ac7a85f5d31af62b6d0f9636eaef31a
Mateus <mateus.bs.rangel@gmail.com>
2020-01-19 17:59:46 -0300

    Update mobile pages and routes

--- mobile/App.js
+++ mobile/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello World!!!</Text>
-    </View>
+    <Routes />
   );
 }
 
 // Cada elemento deve conter sua própria estilização
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    fontWeight: "bold",
-    fontSize: 32,
-  }
-});
