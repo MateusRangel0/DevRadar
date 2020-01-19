d2a4d4959b99480b8f970bdb6596f9b2a9689040
Mateus <mateus.bs.rangel@gmail.com>
2020-01-19 04:01:24 -0300

    Add components and refactor

--- web/src/App.js
+++ web/src/App.js
@@ -6,6 +6,9 @@ import './App.css';
 import './Sidebar.css'
 import './Main.css';
 
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
+
 // 3 conceitos para saber o que é React
 // 1. Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da 
 //                aplicação quando criar? quando tem repetição de um trecho de código ou
@@ -18,28 +21,6 @@ import './Main.css';
 function App() {
   const [devs, setDevs] = useState([]);
 
-  const [github_username, setGithubUsername] = useState('');
-  const [techs, setTechs] = useState('');
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (err) => {
-        console.log(err);
-      },
-      {
-        timeout: 30000,
-      }
-    )
-  }, []);
-
   useEffect(() => {
     async function loadDevs() {
       const response = await api.get('/devs');
@@ -50,18 +31,10 @@ function App() {
     loadDevs();
   }, []);
 
-  async function handleAddDev(e) {
-    e.preventDefault();
+  async function handleAddDev(data) {
 
-    const response = await api.post('/devs', {
-      github_username,
-      techs,
-      latitude,
-      longitude,
-    })
-    setGithubUsername('');
-    setTechs('');
-
+    const response = await api.post('/devs', data)
+    
     setDevs([...devs, response.data]);
   }
 
@@ -69,71 +42,12 @@ function App() {
     <div id ="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev}>
-
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input 
-             name="github_username"
-             id="github_username"
-             required
-             value={github_username}
-             onChange={e => setGithubUsername(e.target.value)}
-             />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input 
-             name="techs" 
-             id="techs" 
-             required
-             value={techs}
-             onChange={e => setTechs(e.target.value)}
-            />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input type="number" 
-              name="latitude" 
-              id="latitude" 
-              required value={latitude}
-              onChange={e => setLatitude(e.target.value)}
-              />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input type="number" 
-              name="longitude" 
-              id="longitude" 
-              required value={longitude}
-              onChange={e => setLongitude(e.target.value)}
-              />
-            </div>
-
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
-
+        <DevForm onSubmit={handleAddDev}/>        
       </aside>
       <main>
         <ul>
           {devs.map(dev => (
-            <li key={dev._id} className="dev-item">
-            <header>
-              <img src={dev.avatar_url} alt={dev.name}/>
-              <div className="user-info">
-                <strong>{dev.name}</strong>
-                <span>{dev.techs.join(', ')}</span>
-              </div>
-            </header>
-            <p>{dev.bio}</p>
-          <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
-          </li>
+            <DevItem key={dev._id} dev={dev} /> 
           ))}
         </ul>
       </main>
