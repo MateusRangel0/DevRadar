74eeda55085ec1325eeaa9b87c9b3eb80ca48f8a
Mateus <mateus.bs.rangel@gmail.com>
2020-01-19 13:34:27 -0300

    Initial commit for mobile application

---
+++ mobile/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello World!!!</Text>
+    </View>
+  );
+}
+
+// Cada elemento deve conter sua própria estilização
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  title: {
+    fontWeight: "bold",
+    fontSize: 32,
+  }
+});
