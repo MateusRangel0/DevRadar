46cb9efc62b80185db0e5882749286be02b31f6b
Mateus <mateus.bs.rangel@gmail.com>
2020-01-20 00:44:02 -0300

    Finalize integration with backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://mateusrangel:semana10@cluster0-qdqgt.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -14,4 +19,4 @@ app.use(cors())
 app.use(express.json()); //tem que vir antes
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -5,6 +5,7 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons';
 
 import api from '../services/api';
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket';
 
 function Main({ navigation }) {
     const [devs, setDevs] = useState([]);
@@ -33,6 +34,22 @@ function Main({ navigation }) {
         loadInitialPosition();
     }, []);
 
+    useEffect(() => {
+        subscribeToNewDevs(dev => setDevs(...devs, dev));
+    }, [devs]);
+
+    function setupWebSocket() {
+        disconnect();
+
+        const { latitude, longitude } = currentRegion;
+
+        connect(
+            latitude,
+            longitude,
+            techs,
+        );
+    }
+
     async function loadDevs() {
         const { latitude, longitude } = currentRegion;
 
@@ -45,10 +62,10 @@ function Main({ navigation }) {
         });
 
         setDevs(response.data.devs);
+        setupWebSocket();
     }
 
     function handleRegionChanged(region) {
-        console.log(region);
         setCurrentRegion(region);
     }
 
